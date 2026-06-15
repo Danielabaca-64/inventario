c902f879b6759be85510b6afba2e20f7f3c06e1c
daniela-64 <dannybaca674@gmail.com>
2026-06-15 10:14:02 -0500

    cambio de conexion

--- src/main/java/util/Conexion.java
+++ src/main/java/util/Conexion.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexion {
 
     private static final String URL =
-            "jdbc:mysql://localhost:3306/inventariotech"
+            "jdbc:mysql://localhost:3305/inventariotech"
             + "?useSSL=false"
             + "&allowPublicKeyRetrieval=true"
             + "&serverTimezone=America/Bogota";
